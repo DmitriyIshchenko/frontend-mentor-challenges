c4b4b16c18f24ce6bcd6cc3400ad1ce6d8cd1b72
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-06-06 16:25:46 +0300

    Create a component to display a filtered list

---
+++ src/JobsList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import JobCard from "./JobCard"
+
+export default function JobsList({ jobs, activeSkills, handleAddSkill }) {
+
+  const content = activeSkills.length === 0 ? jobs : jobs.filter(job => activeSkills.every(skill => job.skills.includes(skill)));
+  const renderedContent = content.map(job => <JobCard key={job.id} job={job} handleAddSkill={handleAddSkill} />)
+
+  return (
+    <ul>
+      {renderedContent}
+    </ul>
+  )
+}
